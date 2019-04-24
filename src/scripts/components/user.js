580c2347127f0c85c80d74b4e8ba52d92c055b50
hacodeml <hacodeml@gmail.com>
2019-04-24 14:13:59 -0500

    crud final final

---
+++ src/scripts/components/user.js
@@ -0,0 +1,5 @@
+export default function User() {
+  console.log('agregaste un usuario!');
+}
+
+
--- src/scripts/ui/modal.js
+++ src/scripts/ui/modal.js
@@ -1,9 +1,17 @@
-export default function Modal (opt) {
+export default function Modal(opt) {
   this.element = opt.element;
+  //function to call Run user from Modal
+  this.referenceToUserFunction = opt.runUser;
+  this.form = opt.element.querySelector('form');
+  this.saveUserBtn = this.form.querySelector('.js_save_user');
+  this.runUserFunction();
 }
 
 Modal.prototype.open = function () {
   console.log('open', this.element);
+  //Reseting current form before displaying it
+  this.form.reset();
+  //Adding show class to modal form to display it
   this.element.classList.add('show');
 };
 
@@ -11,3 +19,19 @@ Modal.prototype.close = function () {
   console.log('close', this.element);
   this.element.classList.remove('show');
 };
+
+Modal.prototype.runUserFunction = function () {
+  this.submit(this.form);
+};
+
+Modal.prototype.innerfoo = function () {
+  console.log('dentro submit');
+}
+
+Modal.prototype.submit = function (form) {
+  form.onsubmit = this.innerfoo();
+};
+
+
+
+
